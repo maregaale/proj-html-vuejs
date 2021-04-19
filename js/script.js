1a18ffce9658b3b860c9a2b8b10168fe1377cde4
Alessandro Marega <ale_marega@hotmail.it>
2021-04-19 18:33:09 +0200

    aggiunta main nav dinamica, creato oggetto dei links della main_nav in vuejs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 const app = new Vue ({
     el: "#root",
     data: {
-
+        mainNavLinks: [
+            {
+                name: "Home",
+                display: true,
+                chevron: true,
+            },
+            {
+                name: "Pages",
+                display: true,
+                chevron: true,
+            },
+            {
+                name: "Courses",
+                display: true,
+                chevron: true,
+            },
+            {
+                name: "Features",
+                display: true,
+                chevron: true,
+            },
+            {
+                name: "Blog",
+                display: true,
+                chevron: true,
+            },
+            {
+                name: "Shop",
+                display: true,
+                chevron: true,
+            }
+        ],
     },
     
 });
